b0980bec6b00697c0637b6bd55bde5351d254a02
niravnayani <niravnayani7@gmail.com>
2019-04-22 20:31:05 -0700

    Meta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <title>Nirav Nayani | Web Developer</title>
+
+        <meta property="og:title" content="Nirav Nayani | Web Developer" />
+        <meta property="og:description" content="Web developer with more than 3 years of experience building killer responsive front-ends. Currently working on various PHP Laravel, Shopify and WordPress projects, focused on both front-end and back-end development." />
+        <meta property="og:image" content="{{ asset('images/background.jpg') }}" />
+
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
@@ -17,8 +23,6 @@
         <link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
-
-        <title>Nirav Nayani | Full-Stack Web Developer</title>
 
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
 
